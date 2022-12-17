3b0c3817432183ccee29b606974d290d36cfd722
Iain Rosen <hello@iainrosen.me>
2022-12-17 11:15:02 -0800

    Added cleanup steps to pytests

--- test_conf.py
+++ test_conf.py
@@ -5,9 +5,10 @@ def test_basic():
     h['hello'] = 'world'
     assert(h['hello']=='world')
     assert(h['version']==executableVersion)
-    os.remove('test.config')
+    os.remove('tests/test.config')
 def test_listing():
     h = config('tests/test.config')
     h['hello']='world'
     assert(h['version']==executableVersion)
     assert(listConfigurations('tests', 'hello')==['world'])
+    os.remove('tests/test.config')
