30c5690628f6d4fc4a2dc651892fcac93581c841
Iain Rosen <hello@iainrosen.me>
2022-12-17 11:12:41 -0800

    first commit

---
+++ betterconfigs.py
@@ -0,0 +1,54 @@
+import pickle
+import os
+executableVersion = "B0.3"
+class config:
+    def __init__(self, path) -> None:
+        self.path = path
+        if os.path.exists(path)==False:
+            try:
+                loadConfig = {}
+                loadConfig['version'] = executableVersion
+                with open(self.path, 'wb') as handle:
+                    pickle.dump(loadConfig, handle, protocol=pickle.HIGHEST_PROTOCOL)
+            except:
+                raise Exception("attempted to initialize the configuration file but was unable to write")
+    def __getitem__(self, key):
+        try:
+            with open(self.path, 'rb') as handle:
+                loadedConfig = pickle.load(handle)
+            return loadedConfig[key]
+        except FileNotFoundError:
+            raise Exception("configuration might not be initialized")
+        except:
+            raise NameError("property doesn't exist in configuration")
+    def __setitem__(self, key, value):
+        try:
+            with open(self.path, 'rb') as handle:
+                loadedConfig = pickle.load(handle)
+            loadedConfig[key] = value
+            with open(self.path, 'wb') as handle:
+                pickle.dump(loadedConfig, handle, protocol=pickle.HIGHEST_PROTOCOL)
+            return 0
+        except FileExistsError:
+            raise Exception("unable to find the configuration")
+        except:
+            raise Exception("error writing configuration file")
+    def __delitem__(self, key):
+        try:
+            with open(self.path, 'rb') as handle:
+                loadedConfig = pickle.load(handle)
+            loadedConfig.pop(key)
+            with open(self.path, 'wb') as handle:
+                pickle.dump(loadedConfig, handle, protocol=pickle.HIGHEST_PROTOCOL)
+            return 0
+        except FileExistsError:
+            raise Exception("unable to find the configuration")
+        except:
+            raise Exception("error writing configuration file or property could not be deleted")
+def listConfigurations(path, key):
+    configFiles = os.listdir(path)
+    configs = []
+    for i in configFiles:
+        configPath = path+"/"+i
+        configs.append(config(configPath)[key])
+    return configs
---
+++ test_conf.py
@@ -0,0 +1,13 @@
+from betterconfigs import *
+import os
+def test_basic():
+    h = config('tests/test.config')
+    h['hello'] = 'world'
+    assert(h['hello']=='world')
+    assert(h['version']==executableVersion)
+    os.remove('test.config')
+def test_listing():
+    h = config('tests/test.config')
+    h['hello']='world'
+    assert(h['version']==executableVersion)
+    assert(listConfigurations('tests', 'hello')==['world'])
