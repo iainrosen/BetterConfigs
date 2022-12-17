f055e587173b323abe58a67eedd017abcbe4fab1
Iain Rosen <hello@iainrosen.me>
2022-12-17 11:15:44 -0800

    Removed listing tests

--- test_conf.py
+++ test_conf.py
@@ -6,9 +6,3 @@ def test_basic():
     assert(h['hello']=='world')
     assert(h['version']==executableVersion)
     os.remove('tests/test.config')
-def test_listing():
-    h = config('tests/test.config')
-    h['hello']='world'
-    assert(h['version']==executableVersion)
-    assert(listConfigurations('tests', 'hello')==['world'])
-    os.remove('tests/test.config')
