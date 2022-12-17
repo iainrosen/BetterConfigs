307776ba831b73b36ee16f1a4db6c9251eaa1767
Iain Rosen <hello@iainrosen.me>
2022-12-17 14:43:41 -0800

    Improved version checking

--- betterconfigs.py
+++ betterconfigs.py
@@ -1,5 +1,6 @@
 import pickle
 import os
+import warnings
 executableVersion = "B0.4"
 class config:
     def __init__(self, path) -> None:
@@ -13,6 +14,8 @@ class config:
             except:
                 raise Exception("attempted to initialize the configuration file but was unable to write")
     def __getitem__(self, key):
+        if self.getVersion()!=executableVersion:
+            raise Exception("version mismatch! run upgradeConfig.")
         try:
             with open(self.path, 'rb') as handle:
                 loadedConfig = pickle.load(handle)
@@ -22,6 +25,8 @@ class config:
         except:
             raise NameError("property doesn't exist in configuration")
     def __setitem__(self, key, value):
+        if self.getVersion()!=executableVersion:
+            raise Exception("version mismatch! run upgradeConfig.")
         try:
             with open(self.path, 'rb') as handle:
                 loadedConfig = pickle.load(handle)
@@ -34,6 +39,8 @@ class config:
         except:
             raise Exception("error writing configuration file")
     def __delitem__(self, key):
+        if self.getVersion()!=executableVersion:
+            raise Exception("version mismatch! run upgradeConfig.")
         try:
             with open(self.path, 'rb') as handle:
                 loadedConfig = pickle.load(handle)
@@ -45,3 +52,21 @@ class config:
             raise Exception("unable to find the configuration")
         except:
             raise Exception("error writing configuration file or property could not be deleted")
+    def getVersion(self):
+        try:
+            with open(self.path, 'rb') as handle:
+                loadedConfig = pickle.load(handle)
+            return loadedConfig['version']
+        except FileNotFoundError:
+            raise Exception("configuration might not be initialized")
+        except:
+            raise NameError("property doesn't exist in configuration")
+def upgradeConfig(path):
+    match config(path)['version']:
+        case "B0.3":
+            warnings.warn("Warning, listConfigurations is depricated.")
+            config(path)['version']=executableVersion
+            return 0
+        case _:
+            raise Exception("unable to upgrade configuration file, version is not included in conversion cases.")
+        
