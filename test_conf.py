df2d03ff6890683a651b5daa63a220c6d0393ad1
Iain Rosen <hello@iainrosen.me>
2022-12-17 16:04:52 -0800

    Added tests for old configuration upgrading

--- test_conf.py
+++ test_conf.py
@@ -6,3 +6,9 @@ def test_basic():
     assert(h['hello']=='world')
     assert(h['version']==executableVersion)
     os.remove('tests/test.config')
+def test_oldconfig():
+    h = config('tests/test.config')
+    h['version']='B0.3'
+    assert(h['version']=='B0.3')
+    assert(upgradeConfig('tests/test.config')==0)
+    os.remove('tests/test.config')
