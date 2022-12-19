3dd60e8ad8b3c5817f113ba10dc0171b8f191b69
Iain Rosen <hello@iainrosen.me>
2022-12-19 11:41:36 -0800

    Added testing for removal of property

--- test_conf.py
+++ test_conf.py
@@ -1,5 +1,6 @@
 from src.betterconfigs import *
 import os
+import pytest
 def test_basic():
     h = config('tests/test.config')
     h['hello'] = 'world'
@@ -12,3 +13,12 @@ def test_oldconfig():
     assert(h['version']=='B0.3')
     assert(upgradeConfig('tests/test.config')==0)
     os.remove('tests/test.config')
+def test_remove():
+    h = config('tests/test.config')
+    h['hello'] = 'world'
+    assert(h['hello']=='world')
+    assert(h['version']==executableVersion)
+    del(h['hello'])
+    with pytest.raises(Exception):
+        h['hello']
+    
